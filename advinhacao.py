ffdd9f8841aadf2f59d3f084d11218576e7b1900
[Alisson Abreu] <[abreu.alisson96@gmail.com]>
2024-03-28 22:01:28 -0300

    adicionando a biblioteca random para utilizar numero secreto aleatorio

--- advinhacao.py
+++ advinhacao.py
@@ -1,5 +1,11 @@
-numero_secreto = 42
-total_de_tentativas = 3
+import random 
+
+print("********************************")
+print("Bem vindo ao jogo de adivinhação")
+print("********************************")
+
+numero_secreto = random.randrange(1,101)
+total_de_tentativas = 30
 
 for rodada in range (1, total_de_tentativas+1): 
 
